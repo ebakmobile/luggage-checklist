132cc5e32c3ad6474e69109283d765be36b59a11
Evan Brodie <ecbrodie@uwaterloo.ca>
2012-09-09 13:37:44 -0400

    Final attempt to revert to bb1bb9296dc5af5485ec0b468def555e51387ae1

--- src/com/lugcheck/TripActivity.java
+++ src/com/lugcheck/TripActivity.java
@@ -2,34 +2,36 @@ package com.lugcheck;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
 public class TripActivity extends Activity {
-
+    int trip_id;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trip);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_suitcase);
+      
+        
+        Bundle extras = getIntent().getExtras();
+        trip_id = extras.getInt("trip_id"); // receiving trip_id from previous intent
+          	
+      String bah= "In Suitcase Class, Trip_id is " + trip_id;
+        Log.w("SDFDS", bah);
+               
+        
+        
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_trip, menu);
-        return true;
-    }
-
+ 
     
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+ 
+    
+    
+    
 
 }
