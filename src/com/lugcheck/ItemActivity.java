5f6122d8232da9e2cad274e69c1d398f7f8e0ca2
liquidfireak <azn.andrew@gmail.com>
2012-09-23 12:13:18 -0400

    Fixed newline issues

--- src/com/lugcheck/ItemActivity.java
+++ src/com/lugcheck/ItemActivity.java
@@ -13,6 +13,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
@@ -33,6 +34,7 @@ public class ItemActivity extends Activity {
 	SQLiteDatabase db; 
 	int suitcase_id;
 	int limit;
+
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,7 +44,7 @@ public class ItemActivity extends Activity {
 		db.setVersion(1);
 		db.setLocale(Locale.getDefault());
 		limit=0;
-		
+	
     	Bundle extras = getIntent().getExtras();
 		suitcase_id = extras.getInt("suitcase_id"); // receiving trip_id from previous activity 
 		String bah= "In Item Class, suitcase_id is " + suitcase_id;
@@ -60,7 +62,7 @@ public class ItemActivity extends Activity {
 		tripContainer.addView(ruler,
 		new ViewGroup.LayoutParams( ViewGroup.LayoutParams.FILL_PARENT, 2));
 		createLayoutsFromDB();
-		
+		c.close();
         
     }//end onCreate
 
@@ -139,17 +141,40 @@ public class ItemActivity extends Activity {
 			});
 
 
-
+			
 			/* Code below handles the situation where u click a item */
-		
+						
 			newTab.setOnClickListener(new Button.OnClickListener() {
-				public void onClick(View v) {
-
-					/* Add slashing in and out code */
+				public void onClick(View view) {
+					
+					LinearLayout layoutText = (LinearLayout) view;
+					TextView textBox = (TextView) layoutText.getChildAt(2);//gets value of textbox 
+					//String itemName = textBox.getText().toString();
+					
+					ImageView im=new ImageView(ItemActivity.this);
+					im.setImageResource(R.drawable.checkmark); // creating a checkmark
+					// FROM STACKOVERFLOW!
+					float d = ItemActivity.this.getResources().getDisplayMetrics().density;
+					int width = (int)(58 * d);
+					int height = (int)(50 * d);
+					im.setLayoutParams(new LayoutParams(width, height));
+					int pad = (int)(5*d);
+					im.setPadding(pad, pad, 0, 0);
 					
 					
 					
-
+					if ((textBox.getPaintFlags() & Paint.STRIKE_THRU_TEXT_FLAG) > 0) //removes slash
+					{	textBox.setPaintFlags( textBox.getPaintFlags() & (~ Paint.STRIKE_THRU_TEXT_FLAG));
+						layoutText.removeViewAt(3); // removes the checkmark icon
+						
+					}
+					
+					else  //adds the slash
+						{textBox.setPaintFlags(textBox.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG); 
+						layoutText.addView(im);
+						
+						}
+					
 				}});
 		}//end while*/
 		c.close();     
