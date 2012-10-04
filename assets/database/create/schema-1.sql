52182d7db093cabbe2ec72d01fc0a8f18f1a26e5
Evan Brodie <ecbrodie@uwaterloo.ca>
2012-10-04 00:13:36 -0400

    Eclipse formatting rules; schema improvement

--- assets/database/create/schema-1.sql
+++ assets/database/create/schema-1.sql
@@ -6,13 +6,15 @@ CREATE TABLE IF NOT EXISTS Trip(
 CREATE TABLE IF NOT EXISTS Suitcase(
 	suitcase_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	suitcase_name TEXT(100) NOT NULL,
-	trip_id INTEGER REFERENCES Trip (trip_id) );
+	trip_id INTEGER REFERENCES Trip (trip_id),
+	UNIQUE (suitcase_name, trip_id));
 
 CREATE TABLE IF NOT EXISTS Item(
 	item_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	item_name TEXT NOT NULL,
 	quantity INTEGER NOT NULL,
-	suitcase_id INTEGER REFERENCES Suitcase(suitcase_id)) ;
+	suitcase_id INTEGER REFERENCES Suitcase(suitcase_id),
+	UNIQUE (item_name, suitcase_id)) ;
 	
 CREATE TABLE IF NOT EXISTS Read(
 	read TEXT PRIMARY KEY);
