838f1d6265f21027cb3687190bee26b08ae08f9b
Andrew Ko <azn.andrew@gmail.com>
2012-10-07 16:56:56 -0400

    going to work on createlayoutsfromdb

--- src/com/lugcheck/AddItemActivity.java
+++ src/com/lugcheck/AddItemActivity.java
@@ -6,8 +6,16 @@ import java.util.Locale;
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AddItemActivity extends Activity {
 	int limit;
@@ -36,19 +44,61 @@ public class AddItemActivity extends Activity {
 			for (int i = 0; i < insertList.size(); i++) {
 				String tempName = insertList.get(i);
 				Log.w("tempName is  ", tempName);
-
+				String INSERT_STATEMENT = "INSERT INTO QuickAdd (name) Values ('" + tempName + "')";
+				db.execSQL(INSERT_STATEMENT); // insert into trip_table db
 			}
 
 		}
-
+		createLayoutsFromDB();
 		c.close();
 
+	}
+
+	public void createLayoutsFromDB() {
+
+		/* Code Below fetches trips from trip_table and creates a layout*/
+		Cursor c = db.rawQuery("SELECT * from QuickAdd", null);
+		c.moveToFirst();
+		while (c.isAfterLast() == false) {
+
+			TextView hw = new TextView(this);
+			String text = c.getString(c.getColumnIndex("name"));
+			hw.setText(text);
+			hw.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+			ImageView im = new ImageView(this);
+			im.setImageResource(R.drawable.opensuitcase);
+			// FROM STACKOVERFLOW!
+			float d = this.getResources().getDisplayMetrics().density;
+			int width = (int) (58 * d);
+			int height = (int) (50 * d);
+			im.setLayoutParams(new LayoutParams(width, height));
+			int pad = (int) (5 * d);
+			im.setPadding(pad, pad, 0, 0);
+			// END
+			int txtPadding = (int) (20 * d);
+			hw.setPadding(0, txtPadding, 0, 0);
+
+			LinearLayout newTab = new LinearLayout(this);
+			newTab.setOrientation(LinearLayout.HORIZONTAL);
+			newTab.addView(im);
+			newTab.addView(hw);
+			newTab.setBackgroundColor(Color.WHITE);
+			LinearLayout tripContainer = (LinearLayout) findViewById(R.id.trips_container);
+			tripContainer.addView(newTab);
+
+			View ruler = new View(this);
+			ruler.setBackgroundColor(Color.BLACK); // this code draws the black lines
+			tripContainer.addView(ruler, new ViewGroup.LayoutParams(
+					ViewGroup.LayoutParams.MATCH_PARENT, 2));
+
+			c.moveToNext();
+
+		}
 	}
 
 	public void addIntoArrayList() {
 		insertList.add("Shoes");
 		insertList.add("Underwear");
-		insertList.add("Shoes");
 		insertList.add("Jacket");
 		insertList.add("Sweater");
 		insertList.add("Pajama");
